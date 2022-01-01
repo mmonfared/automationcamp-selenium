843a23b2b9398553d7957d6618ad83eb0b43cb9e
mohammad.m <mohammad.m@routetitan.com>
2022-01-01 14:39:43 +0400

    Session 7

---
+++ Tests/LoginTests.py
@@ -0,0 +1,42 @@
+from Pages.Login import Login
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from Pages.MainPage import MainPage
+import unittest
+
+
+class LoginTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+
+    def test1(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        login = Login(driver=self.driver)
+        main_page = MainPage(driver=self.driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_on_login_button()
+        main_page.check_main_page()
+        sleep(1)
+
+    def test2(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        login = Login(driver=self.driver)
+        main_page = MainPage(driver=self.driver)
+        login.enter_username("Admin")
+        login.enter_password("admin1234")
+        login.click_on_login_button()
+        main_page.check_main_page()
+        sleep(1)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
+
+
