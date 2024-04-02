2b2ab1a8638ae39645c9f21d46304400caa42263
Mohammad Monfared <74588305+mmonfared@users.noreply.github.com>
2024-04-02 17:39:10 +0400

    Update Session18_Run_In_Open_Session.py

--- Session18_Run_In_Open_Session.py
+++ Session18_Run_In_Open_Session.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 def open_session():
@@ -10,6 +11,7 @@ def open_session():
     service = Service(executable_path=ChromeDriverManager().install())
     options.add_argument("--remote-debugging-port=8484")
     options.add_argument(f"--user-data-dir={user_dir}")
+    options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
@@ -25,9 +27,12 @@ driver = open_session()
 driver.implicitly_wait(3)
 driver.get("http://play2.automationcamp.ir/index.html")
 driver.find_element(By.ID, 'fname').send_keys("Mohammad")
-driver.find_element(By.XPATH, "//input[@id='male']")
-driver.find_element(By.ID, "lastName").send_keys("Monfared")
 
 driver = continue_session()
 driver.find_element(By.XPATH, "//input[@id='male']").click()
 driver.find_element(By.ID, "lname").send_keys("Monfared")
+
+sleep(3)
+
+driver.close()
+driver.quit()
